149827e027aca39c6b86fc2849b5429736b6e214
Vlad <luckyvalenok1@gmail.com>
2022-10-31 02:00:43 +0500

    Конекшены обернуты в try

--- src/main/java/net/luckyvalenok/filemanager/UserRepository.java
+++ src/main/java/net/luckyvalenok/filemanager/UserRepository.java
@@ -6,12 +6,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class UserRepository {
     public static final UserRepository USER_REPOSITORY = new UserRepository();
-    private Connection connection;
     
     public User getUserByCookies(Cookie[] cookies) throws SQLException, ClassNotFoundException {
         String session;
@@ -24,52 +21,57 @@ public class UserRepository {
     }
     
     public User getUser(String filter, String arg) throws SQLException, ClassNotFoundException {
-        PreparedStatement st = getConnection().prepareStatement("SELECT login, password, email FROM users WHERE " + filter + " = ?");
-        st.setString(1, arg);
-        ResultSet rs = st.executeQuery();
-        if (rs.next()) {
-            return new User(rs.getString("login"), rs.getString("password"), rs.getString("email"));
-        } else {
-            return null;
+        try (Connection connection = getConnection()) {
+            PreparedStatement st = connection.prepareStatement("SELECT login, password, email FROM users WHERE " + filter + " = ?");
+            st.setString(1, arg);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                return new User(rs.getString("login"), rs.getString("password"), rs.getString("email"));
+            } else {
+                return null;
+            }
         }
     }
     
     public void addUser(User user) throws SQLException, ClassNotFoundException {
-        PreparedStatement st = getConnection().prepareStatement("INSERT INTO users (login, password, email) VALUES (?, ?, ?)");
-        st.setString(1, user.getLogin());
-        st.setString(2, user.getPassword());
-        st.setString(3, user.getEmail());
-        st.executeUpdate();
+        try (Connection connection = getConnection()) {
+            PreparedStatement st = connection.prepareStatement("INSERT INTO users (login, password, email) VALUES (?, ?, ?)");
+            st.setString(1, user.getLogin());
+            st.setString(2, user.getPassword());
+            st.setString(3, user.getEmail());
+            st.executeUpdate();
+        }
     }
     
     public void addUserBySession(String session, User user) throws SQLException, ClassNotFoundException {
-        PreparedStatement st = getConnection().prepareStatement("UPDATE users SET session = ? WHERE login = ?");
-        st.setString(1, session);
-        st.setString(2, user.getLogin());
-        st.executeUpdate();
+        try (Connection connection = getConnection()) {
+            PreparedStatement st = connection.prepareStatement("UPDATE users SET session = ? WHERE login = ?");
+            st.setString(1, session);
+            st.setString(2, user.getLogin());
+            st.executeUpdate();
+        }
     }
     
     public void removeUserBySession(String session) throws SQLException, ClassNotFoundException {
-        PreparedStatement st = getConnection().prepareStatement("UPDATE users SET session = ? WHERE session = ?");
-        st.setString(1, null);
-        st.setString(2, session);
-        st.executeUpdate();
+        try (Connection connection = getConnection()) {
+            PreparedStatement st = connection.prepareStatement("UPDATE users SET session = ? WHERE session = ?");
+            st.setString(1, null);
+            st.setString(2, session);
+            st.executeUpdate();
+        }
     }
     
     public boolean containsUserByLogin(String login) throws SQLException, ClassNotFoundException {
-        PreparedStatement st = getConnection().prepareStatement("SELECT * FROM users WHERE login = ?");
-        st.setString(1, login);
-        ResultSet rs = st.executeQuery();
-        return rs.next();
+        try (Connection connection = getConnection()) {
+            PreparedStatement st = connection.prepareStatement("SELECT * FROM users WHERE login = ?");
+            st.setString(1, login);
+            ResultSet rs = st.executeQuery();
+            return rs.next();
+        }
     }
     
     private Connection getConnection() throws SQLException, ClassNotFoundException {
-        if (connection != null && !connection.isClosed()) {
-            return connection;
-        }
-        
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database", "root", "");
-        return connection;
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/database", "root", "");
     }
 }
